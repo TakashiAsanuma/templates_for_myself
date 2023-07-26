287328fdd72396d39a11c0316fcf376aada16f2f
TakashiAsanuma <rmacchoj7@gmail.com>
2023-07-26 16:32:14 +0900

    done shfmt to bash script

--- bash_script/template.sh
+++ bash_script/template.sh
@@ -2,9 +2,9 @@
 
 ### 入門モダンLinux P.60より
 ### シェルスクリプトを書いたら >shfmt [filename] でフォーマットしましょう
-set -o errexit   #エラーが起きたときにスクリプトの実行を停止させる
-set -o nounset   #設定されていない変数をエラーとして扱う
-set -o pipefail  #パイプラインの途中でエラーが起きたらパイプライン全体が失敗したとみなす
+set -o errexit  #エラーが起きたときにスクリプトの実行を停止させる
+set -o nounset  #設定されていない変数をエラーとして扱う
+set -o pipefail #パイプラインの途中でエラーが起きたらパイプライン全体が失敗したとみなす
 
 ### デフォルト値を持つコマンドライン引数の例
 firstargument="${1:-somedefaultvalue}"
