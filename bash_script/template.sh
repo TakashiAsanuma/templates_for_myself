64784355ea5cd9f3efef1dd7affc3d4fb520ed15
TakashiAsanuma <rmacchoj7@gmail.com>
2023-07-26 16:15:04 +0900

    Write a template for shell script

---
+++ bash_script/template.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+### 入門モダンLinux P.60より
+### シェルスクリプトを書いたら >shfmt [filename] でフォーマットしましょう
+set -o erexit    #エラーが起きたときにスクリプトの実行を停止させる
+set -o nounset   #設定されていない変数をエラーとして扱う
+set -o pipefail  #パイプラインの途中でエラーが起きたらパイプライン全体が失敗したとみなす
+
+### デフォルト値を持つコマンドライン引数の例
+firstargument="${1:-somedefaultvalue}"
+
+echo "$firstargument"
